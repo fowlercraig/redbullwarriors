656b404a39f4a4614f54211b98f5030eb147b6d9
Craig Fowler <craig@wearegiants.com>
2015-05-26 22:57:09 -0700

    Oi, added featured image.

--- lib/default.php
+++ lib/default.php
@@ -9,41 +9,51 @@
     register_nav_menu('main-menu',__( 'Main Menu' ));
   }
   add_action( 'init', 'register_my_menu' );
+  add_theme_support( 'post-thumbnails' );
 
-  // Register Custom Post Type
   function create_social() {
 
     $labels = array(
-      'name'                => _x( 'Social', 'Post Type General Name', 'text_domain' ),
-      'singular_name'       => _x( 'Social', 'Post Type Singular Name', 'text_domain' ),
-      'menu_name'           => __( 'Social', 'text_domain' ),
-      'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-      'all_items'           => __( 'All Items', 'text_domain' ),
-      'view_item'           => __( 'View Item', 'text_domain' ),
-      'add_new_item'        => __( 'Add New Person', 'text_domain' ),
-      'add_new'             => __( 'Add New', 'text_domain' ),
-      'edit_item'           => __( 'Edit Item', 'text_domain' ),
-      'update_item'         => __( 'Update Item', 'text_domain' ),
-      'search_items'        => __( 'Search Item', 'text_domain' ),
-      'not_found'           => __( 'Not found', 'text_domain' ),
-      'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+      'name'                => _x( 'Social Posts', 'Post Type General Name', 'social' ),
+      'singular_name'       => _x( 'Social Post', 'Post Type Singular Name', 'social' ),
+      'menu_name'           => __( 'Social', 'social' ),
+      'name_admin_bar'      => __( 'Socaial', 'social' ),
+      'parent_item_colon'   => __( 'Parent Item:', 'social' ),
+      'all_items'           => __( 'All Items', 'social' ),
+      'add_new_item'        => __( 'Add New Item', 'social' ),
+      'add_new'             => __( 'Add New', 'social' ),
+      'new_item'            => __( 'New Item', 'social' ),
+      'edit_item'           => __( 'Edit Item', 'social' ),
+      'update_item'         => __( 'Update Item', 'social' ),
+      'view_item'           => __( 'View Item', 'social' ),
+      'search_items'        => __( 'Search Item', 'social' ),
+      'not_found'           => __( 'Not found', 'social' ),
+      'not_found_in_trash'  => __( 'Not found in Trash', 'social' ),
+    );
+    $rewrite = array(
+      'slug'                => 'social',
+      'with_front'          => true,
+      'pages'               => true,
+      'feeds'               => true,
     );
     $args = array(
-      'label'               => __( 'social', 'text_domain' ),
+      'label'               => __( 'social', 'social' ),
+      'description'         => __( 'Social Content', 'social' ),
       'labels'              => $labels,
-      'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', ),
+      'supports'            => array( 'title', 'editor', 'thumbnail', 'post-formats', ),
       'taxonomies'          => array( 'category', 'post_tag' ),
       'hierarchical'        => false,
       'public'              => true,
       'show_ui'             => true,
       'show_in_menu'        => true,
-      'show_in_nav_menus'   => true,
-      'show_in_admin_bar'   => true,
       'menu_position'       => 5,
+      'show_in_admin_bar'   => true,
+      'show_in_nav_menus'   => true,
       'can_export'          => true,
       'has_archive'         => true,
       'exclude_from_search' => false,
       'publicly_queryable'  => true,
+      'rewrite'             => $rewrite,
       'capability_type'     => 'page',
     );
     register_post_type( 'social', $args );
