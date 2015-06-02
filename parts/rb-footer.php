a259977ef54ddef583b573ffc389aaa6a27e9678
Craig Fowler <craig@wearegiants.com>
2015-06-01 19:31:44 -0700

    added footer.

---
+++ parts/rb-footer.php
@@ -0,0 +1,120 @@
+<link href="/cs/RedBull2/stylesheets/shared-modules/shared-footer.20150508121614.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="/cs/RedBull2/stylesheets/shared-modules/shared-footer-desktop.20150508121614.css" media="only screen and (min-width: 640px)" rel="stylesheet" type="text/css" />
+<!--[if lte IE 8]>
+<link href="/cs/RedBull2/stylesheets/shared-modules/shared-footer-IE-lte8.20150508121614.css" media="screen" rel="stylesheet" type="text/css" />
+<![endif]-->
+<script src="/cs/RedBull2/javascripts/vendor/modernizr.custom.20150508121614.js" id="modernizr" type="text/javascript"></script>
+
+
+<footer class='rb-shared-footer' role='contentinfo'>
+<div class='footer-main-nav'>
+<a class='redbull-com' href='http://www.redbull.com' title='Red Bull' target='_blank'>RedBull.com</a>
+<div class='global-social'>
+<h5 class="global-social-title">Follow Red Bull</h5>
+<ul class='global-social-list'>
+<li class='global-social-list__item'>
+<a class='social-icon social-icon--facebook' href='http://www.facebook.com/redbull' target='_blank'>
+<div class='icon-box--20 icon-fallback--facebook-white '>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#facebook'></use>
+</svg>
+</div><span class='social-icon__label'>Facebook</span>
+</a>
+</li>
+<li class='global-social-list__item'>
+<a class='social-icon social-icon--twitter' href='http://twitter.com/#!/redbull' target='_blank'>
+<div class='icon-box--20 icon-fallback--twitter-white '>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#twitter'></use>
+</svg>
+</div><span class='social-icon__label'>Twitter</span>
+</a>
+</li>
+<li class='global-social-list__item'>
+<a class='social-icon social-icon--googleplus' href='https://plus.google.com/+RedBull' target='_blank'>
+<div class='icon-box--20 icon-fallback--googleplus-white '>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#googleplus'></use>
+</svg>
+</div><span class='social-icon__label'>Google+</span>
+</a>
+</li>
+<li class='global-social-list__item'>
+<a class='social-icon social-icon--instagram' href='http://www.instagram.com/redbull' target='_blank'>
+<div class='icon-box--20 icon-fallback--instagram-white '>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#instagram'></use>
+</svg>
+</div><span class='social-icon__label'>Instagram</span>
+</a>
+</li>
+<li class='global-social-list__item'>
+<a class='social-icon social-icon--youtube' href='http://www.youtube.com/redbull' target='_blank'>
+<div class='icon-box--20 icon-fallback--youtube-white '>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#youtube'></use>
+</svg>
+</div><span class='social-icon__label'>YouTube</span>
+</a>
+</li>
+<li class='global-social-list__item'>
+<a class='social-icon social-icon--rss' href='/en/rss/index.rss' target='_blank'>
+<div class='icon-box--20 icon-fallback--rss-white '>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#rss'></use>
+</svg>
+</div><span class='social-icon__label'>RSS</span>
+</a>
+</li>
+</ul>
+</div>
+
+<div class='itunes-footer-promotion'>
+<h5 class='itunes-footer-promotion__title'>Red Bull on</h5>
+<a class='itunes-footer-promotion__link' href='http://win.gs/itunes' target='_blank'>
+<div class='icon-box'>
+<svg>
+<use xlink:href='/cs/RedBull2/images/svg-sprites/icons.20150508121614.svg#inline-itunes'></use>
+</svg>
+</div>
+</a>
+</div>
+</div>
+<div class='footer-meta-nav'>
+<div class='meta-editions'>
+<span class='meta-editions-label'>International Editions:</span>
+<span class='meta-editions-links'>
+<a href='http://www.redbull.com/en' target='_blank'class='is-active'>EN</a>
+<a href='http://www.redbull.com/de' target='_blank'>DE</a>
+<a href='http://www.redbull.com/fr' target='_blank'>FR</a>
+<a href='http://www.redbull.com/es' target='_blank'>ES</a>
+</span>
+</div>
+<div class='meta-local-editions'>
+<span class='meta-local-editions-label'>Local Editions:</span>
+<span class='meta-local-editions-links'>
+<a href='http://www.redbull.com/en/countrypicker' target='_blank'>
+Find out what&apos;s happening in your country</a>
+</span>
+</div>
+<div class='rbwm-meta-info'>
+<p>&copy; 2015 Red Bull</p>
+<ul class='meta-links'>
+<li><a href='/en/data-privacy-policy' title='Data Privacy Policy' target='_parent'><span>Data Privacy Policy</span></a>
+</li>
+<li><a href='/en/terms-and-conditions' title='Terms & Conditions' target='_parent'><span>Terms & Conditions</span></a>
+</li>
+<li><a href='/en/imprint' title='Imprint' target='_parent'><span>Imprint</span></a>
+</li>
+<li><a href='/en/brand-protection' title='Brand Protection' target='_parent'><span>Brand Protection</span></a>
+</li>
+<li><a href='https://www.redbullcontentpool.com' title='Media' target='_parent'><span>Media</span></a>
+</li>
+<li><a href='http://energydrink.redbull.com/contact-landing' title='Contact Us' target='_parent'><span>Contact Us</span></a>
+</li>
+<li><a href='http://jobs.redbull.com/int/en' title='Jobs' target='_parent'><span>Jobs</span></a>
+</li>
+</ul>
+</div>
+</div>
+</footer>
