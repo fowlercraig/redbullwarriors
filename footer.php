dd0f4ab66d145ab27cc49f356ac725729b16af13
Craig Fowler <craig@wearegiants.com>
2015-06-05 13:31:22 -0400

    added a warning for IE users

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer id="footer"></footer>
 </div><!--Wrapper-->
+<!--[if lt IE 8]><script src="/warning/warning.js"></script><script>window.onload=function(){e("/warning")}</script><![endif]-->
 <?php wp_footer(); ?>
 </body>
 </html>
