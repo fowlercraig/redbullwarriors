7f5e768d9f7ed9ecad8ccf7e242c7a9cc9cf49a4
Craig Fowler <craig@wearegiants.com>
2015-06-05 12:47:48 -0400

    respond.js movings

--- header.php
+++ header.php
@@ -14,11 +14,14 @@
 
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+
+<!--[if lt IE 9]>
 <script src="/assets/js/respond.min.js"></script>
 <![endif]-->
 
-
-<?php wp_head(); ?>
 </head>
 
 <?php
