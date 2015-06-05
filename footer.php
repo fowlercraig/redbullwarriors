7fa97309edbd8c0ae6624404ee35daa2567bf051
Craig Fowler <craig@wearegiants.com>
2015-06-05 13:37:02 -0400

    ie warning

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer id="footer"></footer>
 </div><!--Wrapper-->
-<!--[if lt IE 8]><script src="/warning/warning.js"></script><script>window.onload=function(){e("/warning")}</script><![endif]-->
+<!--[if lte IE 6]><script src="/assets/warning/warning.js"></script><script>window.onload=function(){e("/assets/warning/")}</script><![endif]-->
 <?php wp_footer(); ?>
 </body>
 </html>
+
