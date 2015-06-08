3fbdd82b5d3bdd7b02369de54369ba6b536a12a1
Craig Fowler <craig@wearegiants.com>
2015-06-08 16:41:38 -0400

    Added tracking.

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer id="footer"></footer>
 </div><!--Wrapper-->
 <!--[if lte IE 6]><script src="/assets/warning/warning.js"></script><script>window.onload=function(){e("/assets/warning/")}</script><![endif]-->
+<?php include locate_template('parts/home/tracking.php' ); ?>
 <?php wp_footer(); ?>
 </body>
 </html>
---
+++ parts/home/tracking.php
@@ -0,0 +1,43 @@
+<!-- RBMH WRAPPER START -->
+<script type="text/javascript">
+
+  var edWrapperNameSpace = {};
+
+  edWrapperNameSpace.aGTM = [
+    "GTM-LVJ5",   // hq
+    "GTM-7W7M",   // hq
+    "GTM-HPZK",   // hq
+    "GTM-T4VDK2"  // property
+  ];
+
+  var edWrapper = [];
+
+</script>
+
+<script type="text/javascript">
+
+  edWrapperNameSpace.sVers = -1;
+  edWrapperNameSpace.aInitialInnerWrapper = [["downloads.redbull.com/webtrekk/innereWrapper/gtm/innerWrapper_gtm.js",false]];
+  edWrapperNameSpace.sWrapperSrc = ["downloads.redbull.com/webtrekk/edAnalyticsWrapper.js","downloads.redbull.com/webtrekk/confWrapper.js"];
+
+  (function (sVarName) {
+    window.edWrapperNameSpace.sEdWrapperObjectName = sVarName; window[sVarName] = window[sVarName] || [];
+    var script = document.createElement('script'); script.type = 'text/javascript'; script.asyn = false;
+    script.src = ('https:' == document.location.protocol ? 'https://' : 'http://')+edWrapperNameSpace.sWrapperSrc[0] + "?v=" + edWrapperNameSpace.sVers;
+    var script2 = document.getElementsByTagName('script')[0]; script2.parentNode.insertBefore(script, script2);
+  })("edWrapper");
+
+</script>
+<!-- RBMH WRAPPER END -->
+
+<script type="text/javascript">
+
+  edWrapper.push( [ 'trackPageView' , false , {1:'LocalMS' , 2:'LocalMS - nz' , 3:'LocalMS - nz - warriorscan.redbull.com'} , true ] );
+
+  edWrapper.push( [ 'trackPageVar' , 'Language' , 'en'] );
+  edWrapper.push( [ 'trackPageVar' , 'Domain incl. Subdomain' , window.location.hostname] );
+  edWrapper.push( [ 'trackPageVar' , 'Type' , 'LocalMS'] );
+
+  edWrapper.push(['submit']);
+
+</script>
